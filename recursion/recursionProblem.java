2f75d8622b63a5fd1ac3bf0b79c355675c2c7729
yuma <164658568+vertexg@users.noreply.github.com>
2024-11-30 11:13:39 +0900

    leran recursionProblem

--- recursion/recursionProblem.java
+++ recursion/recursionProblem.java
@@ -8,6 +8,7 @@ public class recursionProblem {
     }
     return numberOfDots(x - 1) + x;
   }
+  // ________________________________________________________________________
 
   // totalSquareArea
   public static int totalSquareArea(int x) {
@@ -20,7 +21,7 @@ public class recursionProblem {
   public static String sheeps(int count) {
     return sheepsHelper(count, "");
   }
-
+  // ________________________________________________________________________
   // sheepsCount
 
   public static String sheepsHelper(int count, String str) {
@@ -42,9 +43,39 @@ public class recursionProblem {
     return reverseStringHelper(originalString.charAt(index) + reverseStr, index + 1, originalString);
   }
 
+  // ________________________________________________________________________
+  // countSquare
+  public static int gcd(int x, int y) {
+    if ((x % y) == 0) {
+      return y;
+    } else {
+      return gcd(y, x % y);
+    }
+  }
+
+  public static int countSquare(int x, int y) {
+    return x * y / (int) Math.pow(gcd(x, y), 2);
+  }
+
+  // ________________________________________________________________________
+  public static int splitAndAdd(int digits) {
+    // 関数を完成させてください
+    return splitAndAddHelper(digits, 0);
+  }
+
+  public static int splitAndAddHelper(int digits, int sum) {
+    if (digits == 0) {
+      return sum;
+    }
+    return splitAndAddHelper(digits / 10, sum + digits % 10);
+  }
+
   public static void main(String[] args) {
     System.out.println(numberOfDots(5)); // 15
     System.out.println(totalSquareArea(5)); // 225
     System.out.println(sheeps(2)); // 2 sheep ~ 1 sheep ~
+    System.out.println(reverseString("Hello")); // olleH
+    System.out.println(countSquare(8, 12)); // 24
+    System.out.println(splitAndAdd(12345)); // 15
   }
 }
