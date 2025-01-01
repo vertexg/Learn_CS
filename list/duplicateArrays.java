195330caf7ab25addac6ffc2575560ec442bc751
yuma <164658568+vertexg@users.noreply.github.com>
2025-01-02 07:59:07 +0900

    learn hashmap

--- list/duplicateArrays.java
+++ list/duplicateArrays.java
@@ -1,20 +1,33 @@
 package list;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class duplicateArrays {
   public static int[] intersectionOfArraysRepeats(int[] intList1, int[] intList2) {
     HashMap<Integer, Integer> hashmap = new HashMap<>();
-    for (int i = 0; i < intList2.length; i++) {
-      if (hashmap.get(intList2[i]) == null) {
-        hashmap.put(intList2[i], 1);
-      } else {
-        hashmap.put(intList2[i], hashmap.get(intList2[i]) + 1);
-      }
 
+    for (int i = 0; i < intList2.length; i++) {
+      if (hashmap.get(intList2[i]) == null)
+        hashmap.put(intList2[i], 1);
+      else
+        hashmap.put(intList2[i], hashmap.get(intList2[i]) + 1);
     }
 
-    // 関数を完成させてください
-  }
+    ArrayList<Integer> results = new ArrayList<>();
+    for (int i = 0; i < intList1.length; i++) {
+      if (hashmap.get(intList1[i]) != null && hashmap.get(intList1[i]) > 0) {
+        results.add(intList1[i]);
+        hashmap.put(intList1[i], hashmap.get(intList1[i]) - 1);
+      }
+    }
 
+    int[] result = new int[results.size()];
+    for (int i = 0; i < results.size(); i++) {
+      result[i] = results.get(i);
+    }
+    Arrays.sort(result);
+    return result;
+  }
 }
---
+++ list/hashMapCaching.java
@@ -0,0 +1,29 @@
+package list;
+
+import java.util.HashMap;
+
+public class hashMapCaching {
+  // ハッシュテーブルとは、キーと配列のインデックスを関連付けるために使われるデータ構造
+  // ハッシュテーブルは、キーと値のペアを格納するためのデータ構造で、キーを使って値を検索することができ.
+  // キーに基づく検索や挿入、削除などの操作を効率的に行えるため、非常に便利なデータ構造
+  public static boolean existsWithinList(int[] listL, int dataY) {
+    HashMap<Integer, Integer> hashmap = new HashMap<>();
+
+    // ハッシュマップに格納
+    for (int i = 0; i < listL.length; i++) {
+      hashmap.put(listL[i], listL[i]);
+    }
+
+    // 存在したら true、存在しなければ false
+    return (hashmap.get(dataY) != null) ? true : false;
+  }
+
+  public static void main(String[] args) {
+    // 固定配列を検索します
+    int[] sampleList = { 3, 10, 23, 3, 4, 50, 2, 3, 4, 18, 6, 1, -2 };
+    System.out.println(existsWithinList(sampleList, 23));
+    System.out.println(existsWithinList(sampleList, -2));
+    System.out.println(existsWithinList(sampleList, 100));
+  }
+
+}
