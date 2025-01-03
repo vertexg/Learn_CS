6d2967ed85e9beb5706a5c7c7bbcd6338b8071cb
yuma <164658568+vertexg@users.noreply.github.com>
2025-01-03 23:24:06 +0900

    leran haspmap_problem

---
+++ list/matchMaking.java
@@ -0,0 +1,12 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class matchMaking {
+  public static boolean hasSameType(String user1, String user2) {
+    HashMap<String, String> hashmap = new HashMap<>();
+    // 関数を完成させてください
+
+  }
+}
