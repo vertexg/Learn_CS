80be80df960e8bebfebd198d2d84ac2545f1e180
yuma <164658568+vertexg@users.noreply.github.com>
2025-01-09 22:21:55 +0900

    learn slidingWindow

--- node/slidingWindow.java
+++ node/slidingWindow.java
@@ -2,52 +2,6 @@ package node;
 
 import java.util.ArrayList;
 
-public class slidingWindow {
-  public static ArrayList<Integer> getMaxWindows(int[] arr, int k) {
-    ArrayList<Integer> results = new ArrayList<>();
-    if (k > arr.length)
-      return results;
-
-    Deque deque = new Deque();
-
-    // dequeの初期化
-    for (int i = 0; i < k; i++) {
-
-      while (deque.peekBack() != null && arr[deque.peekBack()] <= arr[i]) {
-        deque.dequeueBack();
-      }
-      deque.enqueueBack(i);
-
-    }
-
-    for (int i = k; i < arr.length; i++) {
-      // dequeの先頭は最大値
-      results.add(arr[deque.peekFront()]);
-
-      // ウィンドウ外にある要素は取り除く
-      while (deque.peekFront() != null && deque.peekFront() <= i - k)
-        deque.dequeueFront();
-      // 現在の値とそれより小さい全てのdequeの値をチェック
-      while (deque.peekBack() != null && arr[deque.peekBack()] <= arr[i])
-        deque.dequeueBack();
-      deque.enqueueBack(i);
-    }
-
-    // 最後のmax
-    results.add(arr[deque.peekFront()]);
-    return results;
-  }
-
-  public static void main(String[] args) {
-
-    // [64, 64, 64, 34, 14, 353, 353, 353, 353, 63]
-    int[] arr = new int[] { 34, 35, 64, 34, 10, 2, 14, 5, 353, 23, 35, 63, 23 };
-    System.out.println(getMaxWindows(arr, 4));
-
-  }
-
-}
-
 class Node {
   public int data;
   public Node prev;
@@ -130,4 +84,50 @@ class Deque {
       this.head = null;
     return temp.data;
   }
+}
+
+class slidingWindow {
+  public static ArrayList<Integer> getMaxWindows(int[] arr, int k) {
+    ArrayList<Integer> results = new ArrayList<>();
+    if (k > arr.length)
+      return results;
+
+    Deque deque = new Deque();
+
+    // dequeの初期化
+    for (int i = 0; i < k; i++) {
+
+      while (deque.peekBack() != null && arr[deque.peekBack()] <= arr[i]) {
+        deque.dequeueBack();
+      }
+      deque.enqueueBack(i);
+
+    }
+
+    for (int i = k; i < arr.length; i++) {
+      // dequeの先頭は最大値
+      results.add(arr[deque.peekFront()]);
+
+      // ウィンドウ外にある要素は取り除く
+      while (deque.peekFront() != null && deque.peekFront() <= i - k)
+        deque.dequeueFront();
+      // 現在の値とそれより小さい全てのdequeの値をチェック
+      while (deque.peekBack() != null && arr[deque.peekBack()] <= arr[i])
+        deque.dequeueBack();
+      deque.enqueueBack(i);
+    }
+
+    // 最後のmax
+    results.add(arr[deque.peekFront()]);
+    return results;
+  }
+
+  public static void main(String[] args) {
+
+    // [64, 64, 64, 34, 14, 353, 353, 353, 353, 63]
+    int[] arr = new int[] { 34, 35, 64, 34, 10, 2, 14, 5, 353, 23, 35, 63, 23 };
+    System.out.println(getMaxWindows(arr, 4));
+
+  }
+
 }
